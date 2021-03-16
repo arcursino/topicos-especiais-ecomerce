abf903a49ba5af455705d650ba7b9d231c7c0932
arcursino <arcursino@gmail.com>
2021-03-16 13:28:35 +0000

    cria repository

---
+++ src/main/java/br/gov/sp/fatec/springbootapp/repository/ClienteRepository.java
@@ -0,0 +1,12 @@
+package br.gov.sp.fatec.springbootapp.repository;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import br.gov.sp.fatec.springbootapp.entity.Cliente;
+
+public interface ClienteRepository extends JpaRepository<Cliente, Long>{  
+
+}
